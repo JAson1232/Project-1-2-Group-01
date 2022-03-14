104acaf548bb8724075315aaf1918cb8488ce5e9
nynzzz <91736289+nynzzz@users.noreply.github.com>
2022-03-14 16:33:59 +0100

    velocityReader and testTXT file

--- core/src/com/mygdx/game/velocityReader.java
+++ core/src/com/mygdx/game/velocityReader.java
@@ -1,12 +1,13 @@
 package com.mygdx.game;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Reader {
+public class velocityReader {
     public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("/Users/mymac/Documents/GitHub/Project-1-2-Group-01/core/src/com/mygdx/game/example_inputfile.txt");
+        File file = new File("/Users/mymac/Documents/GitHub/Project-1-2-Group-01/core/src/com/mygdx/game/VelocityTestTXT.txt");
         Scanner scaner = new Scanner(file);
         int count = 0;
         ArrayList<String> result = new ArrayList<>();
