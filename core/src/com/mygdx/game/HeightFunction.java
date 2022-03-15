c4dcbe21415c3f7e76083ce40e9ef81f41e3cbde
marchen03 <marian.chen@student.maastrichtuniversity.nl>
2022-03-15 20:04:11 +0100

    Minor change for self-reference

--- core/src/com/mygdx/game/HeightFunction.java
+++ core/src/com/mygdx/game/HeightFunction.java
@@ -23,13 +23,14 @@ public class HeightFunction implements Function{
 
     public static String reader() throws FileNotFoundException {
         File file = new File("C:\\Users\\jason\\OneDrive\\Documents\\GitHub\\Project-1-2-Group-01\\core\\src\\com\\mygdx\\game\\example_inputfile.txt");
-        Scanner scaner = new Scanner(file);
+        // File file = new File("C:\\Users\\maria\\OneDrive\\Documents\\GitHub\\Project-1-2-Group-01\\core\\src\\com\\mygdx\\game\\example_inputfile.txt");
+        Scanner scanner = new Scanner(file);
         int count = 0;
         ArrayList<String> result = new ArrayList<>();
 
-        while(scaner.hasNext()) {
+        while(scanner.hasNext()) {
             count++;
-            String word = scaner.next();
+            String word = scanner.next();
 
             if(count%3 == 0){
                 result.add(word);
