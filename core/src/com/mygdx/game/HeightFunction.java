439e9410eb3dee667204d817151ddd08e01aa254
nynzzz <91736289+nynzzz@users.noreply.github.com>
2022-03-15 17:13:36 +0100

    zzz

--- core/src/com/mygdx/game/HeightFunction.java
+++ core/src/com/mygdx/game/HeightFunction.java
@@ -1,9 +1,44 @@
 package com.mygdx.game;
 
+
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class HeightFunction implements Function{
     @Override
-    public double f(double x, double y,double vx,double vy) {
-        return 0;
-        // return  0.5*(Math.sin((x-y)/7)+0.9);
+    public double f(double x, double y,double vx,double vy) throws FileNotFoundException {
+        Expression expression = new ExpressionBuilder(reader())
+                .variables("x", "y")
+                .build()
+                .setVariable("x", x)
+                .setVariable("y", y);
+
+        double result = expression.evaluate();
+
+        return result;
+    }
+
+    public static String reader() throws FileNotFoundException {
+        File file = new File("/Users/mymac/Documents/GitHub/Project-1-2-Group-01/core/src/com/mygdx/game/example_inputfile.txt");
+        Scanner scaner = new Scanner(file);
+        int count = 0;
+        ArrayList<String> result = new ArrayList<>();
+
+        while(scaner.hasNext()) {
+            count++;
+            String word = scaner.next();
+
+            if(count%3 == 0){
+                result.add(word);
+            }
+        }
+        String formula = result.get(7);
+        String newFormula = formula.replace("Math.", "");
+        return newFormula;
     }
 }
