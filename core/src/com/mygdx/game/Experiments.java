cd2d3774bbee63c2bee03d2bfd180bfe2254e6b4
marchen03 <marian.chen@student.maastrichtuniversity.nl>
2022-06-15 03:36:35 +0200

    Experiments

--- core/src/com/mygdx/game/Experiments.java
+++ core/src/com/mygdx/game/Experiments.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 
 public class Experiments {
     static MathFunctions math = new MathFunctions();
+    static double[] stepSizes = {0.1, 0.01, 0.001};
     static double h = 0.01;
     static double time = 3;
     static double epsilon = 0.0;
@@ -14,24 +15,30 @@ public class Experiments {
 //what you want, also set height function to 0
 
     public static void main(String[] args) throws FileNotFoundException {
-        long startTime = 0;
-        startTime = System.nanoTime();
-        for(double i = 0.0; i <= time; i += h) {
-            // state = math.BD2(state, h); // Off values
-            //state = math.AM2(state, h);
-            //state = math.AB3(state, h);
-            //state = math.AB2(state, h);
-            state = math.RK4(state,h);
-            //state = math.RK2(state, h);
-            //state = math.euler(state, h);
-            //System.out.println(state);
-            if((epsilon - 0.000001) <= i && i <= (epsilon + 0.00001)) {
-                System.out.println(state.getX());
-                epsilon += 0.1;
+        for(double stepSize : stepSizes) {
+            h = stepSize;
+            Vector stateCopy = state.clone();
+            long startTime = 0;
+            startTime = System.nanoTime();
+            for (double j = 0.0; j <= time; j += h) {
+                //stateCopy = math.BD2(stateCopy, h); // Off values
+                //stateCopy = math.AM2(stateCopy, h);
+                //stateCopy = math.AB3(stateCopy, h);
+                //stateCopy = math.AB2(stateCopy, h);
+                //stateCopy = math.RK4(stateCopy, h);
+                //stateCopy = math.RK2(stateCopy, h);
+                stateCopy = math.euler(stateCopy, h);
+                //System.out.println(stateCopy);
+                /*
+                if ((epsilon - 0.000001) <= i && i <= (epsilon + 0.00001)) {
+                    System.out.println(state.getX());
+                    epsilon += 0.1;
+                }
+                */
             }
+            long stopTime = System.nanoTime();
+            // System.out.println("Time: " + (stopTime - startTime)/1000000000.000000000);
+            System.out.println("Time: " + (stopTime - startTime) / 1000000000.000000000 + " Step Size: " + h + " Final Position: " + stateCopy);
         }
-        long stopTime = System.nanoTime();
-        // System.out.println("Time: " + (stopTime - startTime)/1000000000.000000000);
-        System.out.println("Time: " + (stopTime - startTime)/1000000000.000000000 + " Step Size: "+ h + " Final Position: " + state);
-    }        
+    }
 }
