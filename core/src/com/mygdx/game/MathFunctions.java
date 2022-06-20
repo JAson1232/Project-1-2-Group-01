801b4c2ebb80ffba4f23b0a1c4e536fd5ef3e4bf
marchen03 <marian.chen@student.maastrichtuniversity.nl>
2022-06-20 23:09:19 +0200

    MathFunctions

--- core/src/com/mygdx/game/MathFunctions.java
+++ core/src/com/mygdx/game/MathFunctions.java
@@ -1,7 +1,5 @@
 package com.mygdx.game;
 
-import com.mygdx.game.Phase3.ModHillClimbing;
-
 import java.io.FileNotFoundException;
 
 
@@ -14,9 +12,14 @@ public class MathFunctions {
 
     public static void main(String[] args) throws FileNotFoundException {
         MathFunctions m = new MathFunctions();
-        Vector state = new Vector(0,0,0,null,1.1,0);
-        Vector newPosition = m.euler(state,5);
-        System.out.println(newPosition.toString());
+
+        Vector state = new Vector(0,0,0,null,1,1);
+        // Vector newPosition = m.euler2(state, 0.1, 0.0, 0.1);
+        // Vector newPosition = m.euler2(state, 0.1, 0.0, 0.2);
+        // Vector newPosition = m.euler2(state, 0.01, 0.0, 0.1);
+        //Vector state = new Vector(0.499260076474938,-8.503256078773477,0,null,0.423622693251916,-1.0294096931284638);
+        Vector newPosition = m.RK2_2(state,0.1, 0.0, 0.2);
+        System.out.println(newPosition);
     }
 
     /***
@@ -28,7 +31,7 @@ public class MathFunctions {
      * @return A linear function derived from two points
      */
     public static LinearFunction linear(double StartX, double StartY,
-                          double EndX, double EndY){
+                                        double EndX, double EndY){
         double slope = (EndY-StartY)/(EndX-StartX);
         double constant = StartY-slope*StartX;
         return new LinearFunction(slope,constant);
@@ -56,11 +59,11 @@ public class MathFunctions {
         accelerationX ax = new accelerationX();
         accelerationY ay = new accelerationY();
         return new Vector (input.getVx(),
-                            input.getVy(),
-                            input.getZ(),
-                            null,
-                            ax.f(input.getX(), input.getY(), input.getVx(), input.getVy()),
-                            ay.f(input.getX(), input.getY(), input.getVx(), input.getVy()));
+                input.getVy(),
+                input.getZ(),
+                null,
+                ax.f(input.getX(), input.getY(), input.getVx(), input.getVy()),
+                ay.f(input.getX(), input.getY(), input.getVx(), input.getVy()));
     }
 
     /**
@@ -75,6 +78,22 @@ public class MathFunctions {
     }
 
     /**
+     * Euler's method but for experiments
+     * @param StateVector
+     * @param h Step size
+     * @return New state
+     * @throws FileNotFoundException
+     */
+    Vector euler2(Vector StateVector, double h, double t0, double tf) throws FileNotFoundException {
+        int N = (int) (tf/h);
+        for(int i = 0; i < N; i++) {
+            //for (double j = t0; j <= tf - h; j += h) {
+            StateVector = StateVector.sum((derivFinder(StateVector).scale(h)));
+        }
+        return StateVector;
+    }
+
+    /**
      * Function for second-order Runge-Kutta (Ralston's second-order method)
      * @param StateVector
      * @param h Step size
@@ -82,12 +101,29 @@ public class MathFunctions {
      * @throws FileNotFoundException
      */
     Vector RK2(Vector StateVector, double h) throws FileNotFoundException {
-        // f(t_i, w_i)
-        Vector k_i_1 = derivFinder(StateVector);
-        // 3f(t_i + 2h/3, w_i + f(t_i, w_i)*2h/3)
-        Vector intermediate = StateVector.sum(k_i_1.scale(2/3.0));
-        Vector k_i_2 = derivFinder(intermediate);
+        Vector k_i_1 = derivFinder(StateVector).scale(h);
+        Vector intermediate = StateVector.sum(k_i_1.scale(2.0/3.0));
+        Vector k_i_2 = derivFinder(intermediate).scale(h);
+        StateVector = StateVector.sum((k_i_1.sum(k_i_2.scale(3.0))).scale(0.25));
         return StateVector.sum((k_i_1.sum(k_i_2.scale(3))).scale(h/4.0));
+    }
+
+    /**
+     * RK2 but for experiments
+     * @param StateVector
+     * @param h Step size
+     * @return New state
+     * @throws FileNotFoundException
+     */
+    Vector RK2_2(Vector StateVector, double h, double t0, double tf) throws FileNotFoundException {
+        int N = (int) (tf / h);
+        for (int i = 0; i < N; i++) {
+            Vector k_i_1 = derivFinder(StateVector).scale(h);
+            Vector intermediate = StateVector.sum(k_i_1.scale(2.0/3.0));
+            Vector k_i_2 = derivFinder(intermediate).scale(h);
+            StateVector = StateVector.sum((k_i_1.sum(k_i_2.scale(3.0))).scale(0.25));
+        }
+        return StateVector;
     }
 
     /**
@@ -98,20 +134,39 @@ public class MathFunctions {
      * @throws FileNotFoundException
      */
     public Vector RK4(Vector StateVector, double h) throws FileNotFoundException {
-        ModHillClimbing.counter1++;
-        // f(t_i, w_i)
+        HillClimbing.counter1++;
         Vector k_i_1 = derivFinder(StateVector).scale(h);
-        // f(t_i + h/2, w_i + k_i_1/2)
         Vector intermediate1 = StateVector.sum(k_i_1.scale(0.5));
         Vector k_i_2 = derivFinder(intermediate1).scale(h);
-        // f(t_i + h/2, w_i + k_i_2/2)
         Vector intermediate2 = StateVector.sum(k_i_2.scale(0.5));
         Vector k_i_3 = derivFinder(intermediate2).scale(h);
-        // f(t_i + h_i, w_i + k_i_3)
         Vector intermediate3 = StateVector.sum(k_i_3);
         Vector k_i_4 = derivFinder(intermediate3).scale(h);
         Vector sum = k_i_1.sum(k_i_2.scale(2)).sum(k_i_3.scale(2)).sum(k_i_4);
         return StateVector.sum(sum.scale(1/6.0));
+    }
+
+    /**
+     * RK4 but for experiments
+     * @param StateVector
+     * @param h Step size
+     * @return New state
+     * @throws FileNotFoundException
+     */
+    Vector RK4_2(Vector StateVector, double h, double t0, double tf) throws FileNotFoundException {
+        int N = (int) (tf / h);
+        for (int i = 0; i < N; i++) {
+            Vector k_i_1 = derivFinder(StateVector).scale(h);
+            Vector intermediate1 = StateVector.sum(k_i_1.scale(0.5));
+            Vector k_i_2 = derivFinder(intermediate1).scale(h);
+            Vector intermediate2 = StateVector.sum(k_i_2.scale(0.5));
+            Vector k_i_3 = derivFinder(intermediate2).scale(h);
+            Vector intermediate3 = StateVector.sum(k_i_3);
+            Vector k_i_4 = derivFinder(intermediate3).scale(h);
+            Vector sum = k_i_1.sum(k_i_2.scale(2.0)).sum(k_i_3.scale(2.0)).sum(k_i_4);
+            StateVector = StateVector.sum(sum.scale(1.0/6.0));
+        }
+        return StateVector;
     }
 
     /**
@@ -134,19 +189,19 @@ public class MathFunctions {
         }
 
         Vector first = new Vector(w_prev.getVx(),
-                                    w_prev.getVy(),
-                                    w_prev.getZ(),
-                                    null,
-                                    ax.f(w_prev.getX(), w_prev.getY(), w_prev.getVx(), w_prev.getVy()),
-                                    ay.f(w_prev.getX(), w_prev.getY(), w_prev.getVx(), w_prev.getVy()))
-                                    .scale(3);
+                w_prev.getVy(),
+                w_prev.getZ(),
+                null,
+                ax.f(w_prev.getX(), w_prev.getY(), w_prev.getVx(), w_prev.getVy()),
+                ay.f(w_prev.getX(), w_prev.getY(), w_prev.getVx(), w_prev.getVy()))
+                .scale(3);
 
         Vector second = new Vector(w_prev2.getVx(),
-                                    w_prev2.getVy(),
-                                    w_prev2.getZ(),
-                                    null,
-                                    ax.f(w_prev2.getX(), w_prev2.getY(), w_prev2.getVx(), w_prev2.getVy()),
-                                    ay.f(w_prev2.getX(), w_prev2.getY(), w_prev2.getVx(), w_prev2.getVy()));
+                w_prev2.getVy(),
+                w_prev2.getZ(),
+                null,
+                ax.f(w_prev2.getX(), w_prev2.getY(), w_prev2.getVx(), w_prev2.getVy()),
+                ay.f(w_prev2.getX(), w_prev2.getY(), w_prev2.getVx(), w_prev2.getVy()));
 
         Vector sum = first.subtract(second).scale(h/2);
         w_prev2 = w_prev;
@@ -177,28 +232,28 @@ public class MathFunctions {
         }
 
         Vector first = new Vector(w_prev.getVx(),
-                                    w_prev.getVy(),
-                                    w_prev.getZ(),
-                                    null,
-                                    ax.f(w_prev.getX(), w_prev.getY(), w_prev.getVx(), w_prev.getVy()),
-                                    ay.f(w_prev.getX(), w_prev.getY(), w_prev.getVx(), w_prev.getVy()))
-                                    .scale(23);
+                w_prev.getVy(),
+                w_prev.getZ(),
+                null,
+                ax.f(w_prev.getX(), w_prev.getY(), w_prev.getVx(), w_prev.getVy()),
+                ay.f(w_prev.getX(), w_prev.getY(), w_prev.getVx(), w_prev.getVy()))
+                .scale(23);
 
         Vector second = new Vector(w_prev2.getVx(),
-                                    w_prev2.getVy(),
-                                    w_prev2.getZ(),
-                                    null,
-                                    ax.f(w_prev2.getX(), w_prev2.getY(), w_prev2.getVx(), w_prev2.getVy()),
-                                    ay.f(w_prev2.getX(), w_prev2.getY(), w_prev2.getVx(), w_prev2.getVy()))
-                                    .scale(16);
+                w_prev2.getVy(),
+                w_prev2.getZ(),
+                null,
+                ax.f(w_prev2.getX(), w_prev2.getY(), w_prev2.getVx(), w_prev2.getVy()),
+                ay.f(w_prev2.getX(), w_prev2.getY(), w_prev2.getVx(), w_prev2.getVy()))
+                .scale(16);
 
         Vector third = new Vector(w_prev3.getVx(),
-                                    w_prev3.getVy(),
-                                    w_prev3.getZ(),
-                                    null,
-                                    ax.f(w_prev3.getX(), w_prev3.getY(), w_prev3.getVx(), w_prev3.getVy()),
-                                    ay.f(w_prev3.getX(), w_prev3.getY(), w_prev3.getVx(), w_prev3.getVy()))
-                                    .scale(5);
+                w_prev3.getVy(),
+                w_prev3.getZ(),
+                null,
+                ax.f(w_prev3.getX(), w_prev3.getY(), w_prev3.getVx(), w_prev3.getVy()),
+                ay.f(w_prev3.getX(), w_prev3.getY(), w_prev3.getVx(), w_prev3.getVy()))
+                .scale(5);
 
         Vector sum = (first.subtract(second).sum(third)).scale(h/12);
         w_prev3 = w_prev2;
@@ -222,34 +277,34 @@ public class MathFunctions {
         if(w_prev == null && w_current == null) {
             // Bootstrap w_(i - 1), w_i
             w_prev = StateVector;
-            w_current = RK4(w_prev, h);  
+            w_current = RK4(w_prev, h);
             return w_current;
         }
 
         w_next = RK4(w_current, h);
-        
+
         Vector first = new Vector(w_next.getVx(),
-                                    w_next.getVy(),
-                                    w_next.getZ(),
-                                    null,
-                                    ax.f(w_next.getX(), w_next.getY(), w_next.getVx(), w_next.getVy()),
-                                    ay.f(w_next.getX(), w_next.getY(), w_next.getVx(), w_next.getVy()))
-                                    .scale(5);
+                w_next.getVy(),
+                w_next.getZ(),
+                null,
+                ax.f(w_next.getX(), w_next.getY(), w_next.getVx(), w_next.getVy()),
+                ay.f(w_next.getX(), w_next.getY(), w_next.getVx(), w_next.getVy()))
+                .scale(5);
 
         Vector second = new Vector(w_current.getVx(),
-                                    w_current.getVy(),
-                                    w_current.getZ(),
-                                    null,
-                                    ax.f(w_current.getX(), w_current.getY(), w_current.getVx(), w_current.getVy()),
-                                    ay.f(w_current.getX(), w_current.getY(), w_current.getVx(), w_current.getVy()))
-                                    .scale(8);
+                w_current.getVy(),
+                w_current.getZ(),
+                null,
+                ax.f(w_current.getX(), w_current.getY(), w_current.getVx(), w_current.getVy()),
+                ay.f(w_current.getX(), w_current.getY(), w_current.getVx(), w_current.getVy()))
+                .scale(8);
 
         Vector third = new Vector(w_prev.getVx(),
-                                    w_prev.getVy(),
-                                    w_prev.getZ(),
-                                    null,
-                                    ax.f(w_prev.getX(), w_prev.getY(), w_prev.getVx(), w_prev.getVy()),
-                                    ay.f(w_prev.getX(), w_prev.getY(), w_prev.getVx(), w_prev.getVy()));
+                w_prev.getVy(),
+                w_prev.getZ(),
+                null,
+                ax.f(w_prev.getX(), w_prev.getY(), w_prev.getVx(), w_prev.getVy()),
+                ay.f(w_prev.getX(), w_prev.getY(), w_prev.getVx(), w_prev.getVy()));
 
         Vector sum = first.sum(second).subtract(third).scale(h/12);
         w_prev = w_current;
@@ -278,14 +333,14 @@ public class MathFunctions {
 
         // Bootstrap w_(i + 1)
         w_next = RK4(w_current, h);
-        
+
         Vector first = new Vector(w_next.getVx(),
-                                    w_next.getVy(),
-                                    w_next.getZ(),
-                                    null,
-                                    ax.f(w_next.getX(), w_next.getY(), w_next.getVx(), w_next.getVy()),
-                                    ay.f(w_next.getX(), w_next.getY(), w_next.getVx(), w_next.getVy()))
-                                    .scale(2*h/3);
+                w_next.getVy(),
+                w_next.getZ(),
+                null,
+                ax.f(w_next.getX(), w_next.getY(), w_next.getVx(), w_next.getVy()),
+                ay.f(w_next.getX(), w_next.getY(), w_next.getVx(), w_next.getVy()))
+                .scale(2*h/3);
 
         w_next = w_current.scale(4/3).subtract(w_prev.scale(1/3)).sum(first);
         w_prev = w_current;
